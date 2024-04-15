c228ee1e0dcfaa5f0ff76ef04df53cece22ad1fa
brenobcsilva100 <brenobcsilva@gmail.com>
2024-04-15 16:13:54 -0300

    first commit

--- src/main/java/com/example/escolareforco/persistence/model/Professor.java
+++ src/main/java/com/example/escolareforco/persistence/model/Professor.java
@@ -1,2 +1,55 @@
-package com.example.escolareforco.persistence.model;public class Professor {
+package com.example.escolareforco.persistence.model;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class Professor {
+
+    private Long id;
+    private String nome;
+    private String turno;
+    private String materia;
+    private Date dataNascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
--- src/main/java/com/example/escolareforco/util/ConverteDataNascimento.java
+++ src/main/java/com/example/escolareforco/util/ConverteDataNascimento.java
@@ -1,2 +1,11 @@
-package com.example.escolareforco.util;public class ConverteDataNascimento {
+package com.example.escolareforco.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ConverteDataNascimento {
+
+    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+//    Date dataFormatada = formato.parse();
+
 }
